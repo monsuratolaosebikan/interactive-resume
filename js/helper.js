50831861cd73270c16f7e8d08e58974956e59125
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2015-09-11 21:32:03 -0400

    Add education and experience formatting

--- js/helper.js
+++ js/helper.js
@@ -1,21 +1,7 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<h3>%data%</h3>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
@@ -29,64 +15,26 @@ var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 var HTMLskills = '<li class="label label-default"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkEmployer = '<h4 class="job-title">%data%';
+var HTMLworkTitle = ' - %data%</h4>';
+var HTMLworkDates = '<div class="date-text label label-default">%data%</div>';
+var HTMLworkLocation = '<div class="location-text"> (%data%)</div><br>';
+var HTMLworkDescription = '<p>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<strong><p class="text-center">%data%</p><strong>';
-var HTMLprojectDates = '<p class="label label-default">%data%</p>';
+var HTMLprojectDates = '<p class="date-text label label-default">%data%</p>';
 var HTMLprojectDescription = '<p><blockquote>%data%</blockquote></p>';
 var HTMLprojectImage = '<div class="col-md-4"><img class="proj-img img-responsive" src="%data%"/></div>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolName = '<h2>%data%';
+var HTMLschoolDegree = ' -- %data%</h2>';
+var HTMLschoolDates = '<p class="date-text label label-default">%data%</p><br>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
-
-var internationalizeButton = '<button>Internationalize</button>';
+var HTMLschoolLogo = '<img class="school-logo" src="%data%"/>';
 var googleMap = '<div id="map"></div>';
-
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-});
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
 
 
 /*
@@ -234,11 +182,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,119 +6,138 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
 var bio = {
-  name:name,
-  role:role,
-  contact: {
-    email: "monsuratolaosebikan@gmail.com",
-    location: "Boston,MA"
-  },
-  skills:["HTML/CSS", "Java", "Javascript", "jQuery", "Bootstrap", "Jasmine Testing", "C++", "Android Development"],
-  welcomeMessage: "Hire me please :)"
-}
+	"name":name,
+	"role":role,
+	"contacts": {
+	"email": "monsuratolaosebikan@gmail.com",
+	"location": "Boston,MA"
+	},
+	"picture": "images/profile_pic.jpg",
+	"skills":["HTML/CSS", "Java", "Javascript", "jQuery", "Bootstrap", "Jasmine Testing", "C++", "Android Development"],
+	"welcomeMessage": "Hire me please :)"
+};
 
-var education={
-    "schools": [
-        {
-            "name": "Central High School",
-            "years": "2009-2013",
-            "degree":"high school diploma",
-            "location":"Bridgeport,CT"
-        },
-        {
-            "name": "Northeastern University",
-            "years": "2013-2018",
-            "degree": "Bachelors",
-            "major": "Mechanical Engineering"
-        }
-    ]
-}
+var education = {
+	"schools": [
+		{
+			"name": "Northeastern University",
+			"years": "2013 - 2018",
+			"degree": "Bachelors",
+			"major": "Mechanical Engineering",
+			"location": "Boston,MA",
+			"logo": "images/neu.png"
+		}
+	]
+};
 
 var work = {
-      "jobs":[
-          {
-            "employer":"GE Transportation",
-            "title":"Advanced Manufacturing Co-op",
-            "location":"Erie,PA",
-            "dates": "August 2015-December 2015",
-            "description": "I played with a 3D printer"
-          },
-          {
-            "employer":"GE Transportation",
-            "title":"Inventory Management Intern",
-            "location":"Erie,PA",
-            "dates": "May 2015-August 2015",
-            "description": "I played with parts"
-          }
-
-]
-
-}
+	"jobs": [
+		{
+			"employer":"GE Transportation",
+			"title":"Advanced Manufacturing Co-op",
+			"location":"Erie,PA",
+			"dates": "August 2015 - December 2015",
+			"description": "Managed a 3D printer"
+		},
+		{
+			"employer":"GE Transportation",
+			"title":"Inventory Management Intern",
+			"location":"Erie,PA",
+			"dates": "May 2015 - August 2015",
+			"description": "Counted parts and verified inventory management system accuracy"
+		}
+	]
+};
 
 var projects = {
-  "projects":[
-    {
-      "title":"Novel Android App",
-      "dates":"May 2014",
-      "description":["This is an android app I designed that allows you to scan books and gives you important information like the average rating, title, author, number of pages,",
-		              "and a little description about the book. I love to read and sometimes I just peruse my local bookstore for interesting finds that I may not want to buy at the",
-		              "time but need a way of remembering them for later."],
-      "image":"images/novel_app_half.png"
-    },
-    {
-    	"title":"RSS FeedReader Testing",
-    	"dates":"September 2015",
-    	"description":["In this project I was given a web-based application that reads RSS feeds and used the Jasmine Test Framework to test the functionality of the RSS Reader.",
-				    	"The tests included checking if there was a defined name and url for each feed, if the menu was hidden upon opening the webpage, if the menu open and closed when the menu",
-				    	"icon was clicked, and if an asynchronous function successfully changed the html content dynamically when called" ],
-    	"image":"images/jasmine_testing.jpe"
-    }
-  ]
-}
-
-if(bio.skills.length>0){
-  for(var i=0;i<bio.skills.length;i++){
-  var formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
-  $("#skills").append(formattedSkill);
-}
-}
-var displayWork = function(){
-for(job in work.jobs ){
-  $("#workExperience").append(HTMLworkStart);
-  var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-  var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
-  var formattedEmployerTitle = formattedEmployer + formattedTitle;
-  $(".work-entry:last").append(formattedEmployerTitle);
-  var formattedworkDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
-  $(".work-entry:last").append(formattedworkDates);
-  var formattedworkDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
-  $(".work-entry:last").append(formattedworkDescription);
-
-}
+	"projects":[
+		{
+			"title":"Novel Android App",
+			"dates":"May 2014",
+			"description":["This is an android app I designed that allows you to scan books and gives you important information like the average rating, title, author, number of pages,",
+							"and a little description about the book. I love to read and sometimes I just peruse my local bookstore for interesting finds that I may not want to buy at the",
+							"time but need a way of remembering them for later."],
+			"image":"images/novel_app_half.png"
+		},
+		{
+			"title":"RSS FeedReader Testing",
+			"dates":"September 2015",
+			"description":["In this project I was given a web-based application that reads RSS feeds and used the Jasmine Test Framework to test the functionality of the RSS Reader.",
+							"The tests included checking if there was a defined name and url for each feed, if the menu was hidden upon opening the webpage, if the menu open and closed when the menu",
+							"icon was clicked, and if an asynchronous function successfully changed the html content dynamically when called." ],
+			"image":"images/jasmine_testing.jpe"
+		}
+	]
 };
 
+var displayBio = function() {
+	var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
+	$(".profile").append(formattedPic);
+	if(bio.skills.length>0) {
+		for(var i=0;i<bio.skills.length;i++) {
+			var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+			$("#skills").append(formattedSkill);
+		}	
+	}
+};
+
+var displayWork = function() {
+	if(work.jobs.length > 0) {
+		for(job in work.jobs ) {
+			$("#workExperience").append(HTMLworkStart);
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+			var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+			var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedLocation;
+			$(".work-entry:last").append(formattedEmployerTitle);
+			var formattedworkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+			$(".work-entry:last").append(formattedworkDates);
+			var formattedworkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+			$(".work-entry:last").append(formattedworkDescription);
+		}
+	}
+};
+
+
+var displayProjects = function() {
+	if(projects.projects.length > 0) {
+		for(project in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			$("#projDesc").append(formattedDates);
+			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description.join(" "));
+			$("#projDesc").append(formattedDescription);
+			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			$(".project-entry:last").append(formattedTitle);
+			var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].image);
+			$(".project-entry:last").append(formattedImage);
+			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		}
+	}
+};
+
+var displayEducation = function() {
+	if(education.schools.length > 0) {
+		for(school in education.schools) {
+			$("#education").append(HTMLschoolStart);
+			var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+			$(".education-entry:last").append(formattedName + formattedDegree);
+			var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+			$(".education-entry:last").append(formattedMajor);
+			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+			$(".education-entry:last").append(formattedLocation);
+			var formattedYears = HTMLschoolDates.replace("%data%", education.schools[school].years);
+			$(".education-entry:last").append(formattedYears);
+			var formattedLogo = HTMLschoolLogo.replace("%data%", education.schools[school].logo);
+			$(".education-entry:last").append(formattedLogo);
+		}
+	}
+};
+
+displayBio();
+displayProjects();
 displayWork();
-
-$(document).click(function(loc) {
-  var x=loc.pageX;
-  var y=loc.pageY;
-  logClicks(x,y);
-});
-
-var projectsDisplay = function(){
-  for(project in projects.projects){
-    $("#projects").append(HTMLprojectStart);
-    var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
-    $("#projDesc").append(formattedDates);
-    var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description.join(" "));
-    $("#projDesc").append(formattedDescription);
-    var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
-    $(".project-entry:last").append(formattedTitle);
-    var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].image);
-    $(".project-entry:last").append(formattedImage);
-    var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
- }
-};
-
-projectsDisplay();
+displayEducation();
 
 $("#mapDiv").append(googleMap);
